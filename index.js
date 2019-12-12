a5474c2f62bbd0712a901fbe4199cf565c352cd5
Binodmagar <ranabinod615@gmail.com>
2019-12-12 07:40:25 +0545

    [#8]post request code of register, login and delete user are written

--- index.js
+++ index.js
@@ -1,23 +1,37 @@
-const Sequelize = require('sequelize');
+var express = require('express')
+var bodyParser = require('body-parser')
 
-//passing parameters separately
-const sequelize = new Sequelize('webapi', 'root', 'admin', {
-	host: 'localhost',
-	dialect: //this means database that we are going to use (mysql)
-});
+//for image
+var multer = require('multer');
+var upload = multer({dest: 'images/'})
 
-sequelize
-.authenticate()
-//then and catch means promise handler this means sequence of work to be done
-//threee state pending, resloved and rejected
-// var a = 'ram';
-// console.log(a);
-.then(() => {
-	console.log('Connection success');
-})
-.catch(function (err){//this means function of error and same as down below
-	console.log(err);
-});
-//.catch(err => {
-	//console.error('unable to connect:', err);
-//});
+var app = express() 
+
+// app.post('/test', upload.single('image'), function(req, res, next){
+// 	console.log(req.file);
+// 	console.log(req.body);
+// })//for image
+
+// var dbConfig = require('./Config/databaseConfig.js')
+// console.log(dbConfig.sequelize)
+
+var userModel = require('./Models/userMode.js')
+var userController = require('./Controller/userController.js')
+//forlogin
+var authController = require('./Controller/AuthController.js')
+
+
+app.use(bodyParser.urlencoded({extended: true }));
+
+
+//for register
+app.post('/register',userController.validator, userController.CheckIfUserExit, userController.getHash, userController.actualRegister)//defining route
+// console.log(req.body);
+
+//for login
+app.post('/login',authController.validator, authController.passwordChecker, authController.jwtTokenGen)
+// console.log(req.body);
+
+app.delete('/user/:id',userController.deleteUser)
+
+app.listen(3003);
